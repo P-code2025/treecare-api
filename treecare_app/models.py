a25dc8a5840b1a75674ddbef7a570c9f8ed2e35a
P-code2025 <phu.b24cc225@stu.ptit.edu.vn>
2025-09-18 21:05:40 +0700

    Add Tree fixtures

--- treecare_app/models.py
+++ treecare_app/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Tree(models.Model):
-    UploadImage = models.ImageField(upload_to='tree_images/')
+    UploadImage = models.ImageField(upload_to='tree_images/', max_length=255)
     Result = models.CharField(max_length=255)
     Species = models.CharField(max_length=100, blank=True, null=True)
     Disease = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.Result
-        
